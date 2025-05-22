cd8deffe43186441e9b72783aaa2662d438c4e3a
Daniel Castillo <danielcastillobalboa@gmail.com>
2025-05-22 11:13:00 +0100

    feat: add historia page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vue from '@astrojs/vue';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: vercel()
 });
